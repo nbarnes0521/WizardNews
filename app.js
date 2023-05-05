d378ea70d66850393cfafb968ecd6f7c43cf3c31
Nick <Nbarnes0521@gmail.com>
2023-05-04 20:07:40 -0500

    Single page posts

--- app.js
+++ app.js
@@ -1,10 +1,57 @@
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
+const postBank = require("./postBank");
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.use(express.static('public'));
+
+app.use(morgan('dev'));
+// app.get("/", (req, res) => res.send("Hello World!"));
+
+
+app.get('/posts/:id', (req, res) => {
+  const id = req.params.id;
+  const post = postBank.find(id);
+  res.send(
+    
+  );
+});
+
+app.get("/", (req, res,) => {
+  
+  const posts = postBank.list();
+
+  const html = `<!DOCTYPE html>
+  <html>
+  <head>
+    <title>Wizard News</title>
+    <link rel="stylesheet" href="/style.css" />
+  </head>
+  <body>
+    <div class="news-list">
+      <header><img src="/logo.png"/>Wizard News</header>
+      ${posts.map(post => `
+        <div class='news-item'>
+          <p>
+            <span class="news-position">${post.id}. ▲</span>
+            ${post.title}
+            <small>(by ${post.name})</small>
+          </p>
+          <small class="news-info">
+            ${post.upvotes} upvotes | ${post.date}
+          </small>
+        </div>`
+      ).join('')}
+    </div>
+  </body>
+</html>`;
+
+  res.send(html);
+});
 
 const PORT = 1337;
 
 app.listen(PORT, () => {
   console.log(`App listening in port ${PORT}`);
 });
+
